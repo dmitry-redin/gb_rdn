f76da8814de9f595b498f582a8d79a4a3af69806
Dmitry Redin <>
2023-04-26 13:15:27 +0300

    Lesson 1

---
+++ Znakomstvo/Lesson1/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+int m = a > b ? a : b;
+
+Console.WriteLine(m);
---
+++ Znakomstvo/Lesson1/Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int m = a > b && a > c ? a : b > c && b > a ? b : c;
+
+Console.WriteLine(m);
