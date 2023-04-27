1dd1e215953d6d801ced9860dcc86369445d9482
Dmitry Redin <>
2023-04-27 18:46:12 +0300

    Lesson 2

---
+++ Znakomstvo/Lesson2/Task13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int numberCount(int num)
+{
+    int c = 1;
+
+    while(num > 0)
+    {
+        num /= 10;
+        if(num !=0)
+        {
+            c++;
+        }
+    }
+
+    return c;
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int f = numberCount(n);
+
+if(f >= 3)
+{
+    Console.WriteLine((n/((int)Math.Pow(10,(f-3))))%10);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
---
+++ Znakomstvo/Lesson2/Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+bool isWeekend(int day)
+{
+    return day == 6 || day == 7;
+}
+
+int d = Convert.ToInt32(Console.ReadLine());
+
+if(isWeekend(d))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
