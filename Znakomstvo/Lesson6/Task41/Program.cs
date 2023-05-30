560c8f81fa7e0f107b765dfc2bdb74178538a5a8
Dmitry Redin <>
2023-05-30 13:57:41 +0300

    Lesson 6

---
+++ Znakomstvo/Lesson6/Task41/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int GetInt()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GetArray(int len)
+{
+    var arr = new int[len];
+
+    int i = 0;
+
+    while(i < len)
+    {
+        arr[i] = GetInt();
+        i++;
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach(var a in arr)
+    {
+        Console.Write(a.ToString() + " ");
+    }
+
+    Console.WriteLine();
+}
+
+int FindPositive(int[] arr)
+{
+    int pos = 0;
+
+    foreach(var a in arr)
+    {
+        if(a > 0) pos++;
+    }
+
+    return pos;
+}
+
+int len = GetInt();
+
+var arr = GetArray(len);
+
+PrintArray(arr);
+
+Console.WriteLine(FindPositive(arr));
+
+
---
+++ Znakomstvo/Lesson6/Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// a*x + c = b*x + d
+using static System.Convert;
+using static System.Console;
+
+double GetDouble()
+{
+    return ToDouble(ReadLine());
+}
+
+double[] Intersection(double a, double c, double b, double d)
+{
+    return new double[2]{(d-c)/(a-b),a*((d-c)/(a-b)) + c};
+}
+
+void PrintPoint(double[] point)
+{
+    WriteLine("{0:F2}, {1:F2}", point[0], point[1]);
+}
+
+var a = GetDouble();
+var c = GetDouble();
+var b = GetDouble();
+var d = GetDouble();
+
+PrintPoint(Intersection(a,c,b,d));
